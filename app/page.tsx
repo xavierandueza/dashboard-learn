763912160bd417247a0387f606ece9253de0629b
Xavier Andueza <xand0001@student.monash.edu>
2023-11-22 21:28:48 +1100

    added something to hopefully solve build issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import type { ImageLoaderProps } from 'next/image';
 
 export default function Page() {
   return (
